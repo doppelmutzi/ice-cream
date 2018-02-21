9472278faaf53bd2611d1d71abfe86b9fbd21e78
Sebastian Weber <sebastian.weber@diva-e.com>
2018-02-21 23:21:08 +0100

    Adding React Native Router

--- App.js
+++ App.js
@@ -1,22 +1,15 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Scene, Router } from "react-native-router-flux";
+import Login from "./Login";
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text> Changes you make will automatically reload. </Text>
-        <Text> Shake your phone to open the developer menu. </Text>
-      </View>
+      <Router>
+        <Scene key="root">
+          <Scene key="login" component={Login} title="Login" />
+        </Scene>
+      </Router>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
-  }
-});
